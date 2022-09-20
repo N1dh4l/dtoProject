bf7d76894e6ad15159d4b7c015ec2c27afaaa9a9
Nidhal Tarhouni <ntarhouni@oula.tn>
2022-09-20 11:11:58 +0200

    create project dto

---
+++ src/main/java/dev/dto/dtoApp/DtoAppApplication.java
@@ -0,0 +1,13 @@
+package dev.dto.dtoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtoAppApplication.class, args);
+	}
+
+}
